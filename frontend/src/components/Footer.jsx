4f5f371e027ef25dfa9b33809bf2b07a6c335701
carlos <carlosperez505@gmail.com>
2024-10-14 20:32:21 -0600

    Removed captcha and redid footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -35,9 +35,7 @@ const Footer = () => {
 
             <div className="mt-6 text-sm max-w-md mx-auto">
                 <p>
-                    Disclaimer: a lucky winner will be chosen at random. No purchase necessary to enter or win.
-                    The odds of winning depend on the number of entries received. Winners will be contacted via
-                    email or social media. By participating, you agree to comply with all terms and conditions.
+                    Thank you for visiting!
                 </p>
             </div>
         </footer>
