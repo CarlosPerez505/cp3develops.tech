bd0bf026e4fd1003874dfa8b4128431319938ac7
carlos <carlosperez505@gmail.com>
2024-10-16 08:19:34 -0600

    Updated about section

--- frontend/src/components/AboutMe.jsx
+++ frontend/src/components/AboutMe.jsx
@@ -39,25 +39,39 @@ const AboutMe = () => {
                 <div className="text-white text-left lg:text-left">
                     <h2 className="text-3xl font-bold mb-4 text-purple-400">Who am I?</h2>
                     <p className="text-lg mb-4 leading-relaxed">
-                        I'm a dedicated and motivated full stack web developer/engineer who builds mobile first, interactive  web sites and web applications. With over 11 years of professional mechanical experience, including work as a mechanic in the Army and at Baker Hughes. My background in mechanics has laid a strong foundation for my career as a engineer, where I apply my problem-solving skills and attention to detail to build innovative web applications.
+                        I'm a dedicated and motivated full stack web developer/engineer who builds mobile first,
+                        interactive web sites and web applications. With over 11 years of professional mechanical
+                        experience, including work as a mechanic in the Army and at Baker Hughes. My background in
+                        mechanics has laid a strong foundation for my career as a engineer, where I apply my
+                        problem-solving skills and attention to detail to build innovative web applications.
                     </p>
                     <p className="text-lg mb-4 leading-relaxed">
-                        I am currently working on <strong className="text-purple-400">The Red Palm Project</strong>, a web app designed to raise awareness and help locate missing Indigenous people in the Southwest using advanced mapping and data analysis techniques. I strive to make a meaningful impact on real-world problems by leveraging technology.
+                        I am currently working on <strong className="text-purple-400">The Red Palm Project</strong>, a
+                        robust and blazingly fast progressive web app (PWA) built as a Single Page Application (SPA). It
+                        features a custom Content Management System (CMS) that allows administrators to seamlessly
+                        update the database of missing persons. The Red Palm Project is designed to raise awareness and
+                        assist in locating missing Indigenous people in the Southwest by leveraging advanced mapping
+                        technologies, data analysis techniques, and real-time visualizations. This platform also
+                        integrates modern SaaS (Software as a Service) principles, which deliver applications over the
+                        internet as a service, eliminating the need for complex infrastructure on the user’s side. I
+                        strive to make a meaningful impact on real-world problems by leveraging technology, building
+                        scalable, efficient solutions that address critical social issues.
                     </p>
+
                     <blockquote className="italic text-purple-300 mt-4">
                         "Striving to make a meaningful impact through technology."
                     </blockquote>
                     <div className="flex space-x-6 mt-6 justify-center">
                         <div className="flex flex-col items-center hover:scale-110 transition-transform duration-200">
-                            <FaCode className="text-3xl text-purple-400" />
+                            <FaCode className="text-3xl text-purple-400"/>
                             <span className="text-sm mt-1 text-white">Web Dev</span>
                         </div>
                         <div className="flex flex-col items-center hover:scale-110 transition-transform duration-200">
-                            <FaChess className="text-3xl text-purple-400" />
+                            <FaChess className="text-3xl text-purple-400"/>
                             <span className="text-sm mt-1 text-white">Chess</span>
                         </div>
                         <div className="flex flex-col items-center hover:scale-110 transition-transform duration-200">
-                            <FaGlobe className="text-3xl text-purple-400" />
+                            <FaGlobe className="text-3xl text-purple-400"/>
                             <span className="text-sm mt-1 text-white">Jiu-Jitsu</span>
                         </div>
                     </div>
