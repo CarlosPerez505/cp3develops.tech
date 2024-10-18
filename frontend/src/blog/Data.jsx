793c2fe51aa6334ed9bb5a1312e1546e12fcdab4
carlos <carlosperez505@gmail.com>
2024-10-17 20:01:12 -0600

    Added some content to blog

--- frontend/src/blog/Data.jsx
+++ frontend/src/blog/Data.jsx
@@ -3,13 +3,13 @@ export const posts = [
     {
         id: 1,
         title: 'First Blog Post',
-        excerpt: 'This is the first blog post excerpt...',
+        excerpt: 'Welcome to my tech and other stuff blog, stay tuned for more content as you read about my life life in New Mexico.' ,
         content: 'Full content of the first blog post...',
     },
     {
         id: 2,
         title: 'Second Blog Post',
-        excerpt: 'This is the second blog post excerpt...',
+        excerpt: 'My coding journey began during the pandemic...',
         content: 'Full content of the second blog post...',
     },
 ];
